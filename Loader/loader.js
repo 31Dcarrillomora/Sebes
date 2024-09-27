c469fd6a785a5c237b0e72af4992b22093951f39
Diego Alexis Carrillo Mora <dcarrillomora31@gmail.com>
2024-09-27 18:14:41 -0500

    Sebes1

---
+++ Loader/loader.js
@@ -0,0 +1,5 @@
+  window.addEventListener('load', function() {
+    setTimeout(function() {
+      document.body.classList.add('loaded');
+    }, 200); // Cambiar este valor para el tiempo de animación
+  });
